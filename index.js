5a5abf3924a3ff7ecc95290d82882485b7ba16dd
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-27 14:34:55 -0400

    getting all questions listed

--- index.js
+++ index.js
@@ -8,21 +8,57 @@ const questions = [];
 inquirer
   .prompt([
     {
-      type: 'input',
-      message: 'What is your name?',
-      name: 'name',
+        type: "input",
+        message: "Hello, what is your name?",
+        name: "name",
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      choices: ['one', 'two', 'three'],
-      name: 'lanaguages'
+        type: "input",
+        message: "Nice to meet you {NAME}. What's your new project called?",
+        name: "title"
     },
     {
-      type: 'input',
-      message: 'What is your preferred method of communication?',
-      name: 'communication',
+        type: "input",
+        message: "Sounds amazing. Can you tell me a bit more about the project? Try to answer the following questions: What was your motivation? What problem does it solve? What's unique about it?'",
+        name: "description",
     },
+    {                               
+        type: "checkbox",
+        message: "Do you want to add a Table of Contents?",
+        choices: ["yes", "no"],
+        name: "toc"
+
+    },
+    {
+        type: "input",
+        message: "What steps can be taken to install your project?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "After installalation, how can I use it? Also, how does someone use the app in the real world?",
+        name: "usage"
+    },  
+    {
+        type: "input",
+        message: "What kind of licensing does this project have? Are there any restrictions on how it can be used?",
+        name: "licensing"
+    }, 
+    {
+        type: "input",
+        message: "Any notable contributors you want to mention? How can other contribute to the project in the future?",
+        name: "contributions"
+    }, 
+    {
+        type: "input",
+        message: "Are their any tests written for this project? How can additional tests be written by others?",
+        name: "testing"
+    }, 
+    {
+        type: "input",
+        message: "Where can questions be asked?",
+        name: "questions"
+    },   
   ])
   .then((response) =>
   console.log(response)
