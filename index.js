d99d343dc7da3537a2e1a5ca56b4779a0a78c1de
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-27 14:04:13 -0400

    setting up initial questioning

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      choices: ['one', 'two', 'three'],
+      name: 'lanaguages'
+    },
+    {
+      type: 'input',
+      message: 'What is your preferred method of communication?',
+      name: 'communication',
+    },
+  ])
+  .then((response) =>
+  console.log(response)
+    // fs.writeFile( `${data.name}.json`, JSON.stringify(response) )
+  );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
