7784e45609d66aa665cdc750a7e8b2c96cebc579
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-28 13:04:52 -0400

    working on init function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
     {
         type: "input",
@@ -13,6 +13,7 @@ const questions = [
         type: "input",
         message: "What's your Github Username?",
         name: "username",
+        // Require a Username (without checking for validity of given Username)
         validate: async (input) => {
             if (!input) {
                return 'Please enter a Github Username..';
@@ -24,12 +25,7 @@ const questions = [
         type: "input",
         message: "What is your email address?",
         name: "email",
-        // validate: async (input) => {
-        //     if (!input) {
-        //        return 'Please enter an email address..';
-        //     }
-        //     return true;
-        //  },
+        // Require an email address (without checking for validity of given email address beyond requiring email@domain formatting)
         validate: function(email) {
             // Regex mail check (return true if valid mail)
             // Credit to https://gist.github.com/Amitabh-K for the email regex below
@@ -38,8 +34,9 @@ const questions = [
     },
     {
         type: "input",
-        message: "Nice to meet you {NAME}. What's your new project called?",
+        message: "What's your project called?",
         name: "title",
+        // Require a Title
         validate: async (input) => {
             if (!input) {
                return 'Every great project starts with a great Title. I must insist.';
@@ -86,6 +83,7 @@ inquirer
   .then((response) => {
     // Define individual responses
     var name = response.name;
+    // Only show By line if Name is not empty
     if (!name) {
         name = "";
     } else {
@@ -95,46 +93,56 @@ inquirer
     var email = "You can reach me at " + response.email.toLowerCase() + " if you have any questions.";
     var title = response.title;
     var description = response.description;
+    // Show "TBD" if Description is empty
     if (!description) {
         description = "TBD";
     };
     var toc = "[Installation](#installation)<br />[Usage](#usage)<br />[License](#license)<br />[Contributing](#contributing)<br />[Tests](#tests)<br />[Questions](#questions)";
     var installation = response.installation;
+    // Show "TBD" if Installation is empty
     if (!installation) {
         installation = "TBD";
     };
     var usage = response.usage;
+    // Show "TBD" if Usage is empty
     if (!usage) {
         usage = "TBD";
     };
     var licensing = "This project is licensed under the " + response.license + " license.";
     var licenseDesc = "";
     var badge = "";
+    // Show GNU Licence if chosen
     if (licensing === "This project is licensed under the GNU GPLv3 license.") {
         badge = "\n\n[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n";
         licenseDesc = "\nPermissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.<p />For more information visit https://choosealicense.com/licenses/gpl-3.0/.";
+       // Show MIT Licence if chosen
     } else if (licensing === "This project is licensed under the MIT license.") {
         badge = "\n\n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n";
         licenseDesc = "\nA short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.<p />For more information visit https://choosealicense.com/licenses/mit/.";
+        // Show No Licence if chosen
     } else if (licensing === "This project is licensed under the None license.") {
         badge = "";
         licenseDesc = "";
         licensing = "This project is not currently licensed.";
     };
     var contributing = response.contributing;
+    // Show "TBD" if Contributing is empty
     if (!contributing) {
         contributing = "TBD";
     };
     var tests = response.tests;
+    // Show "TBD" if Tests is empty
     if (!tests) {
         tests = "TBD";
     };
+    // Build readMe string based on answers
     var readMe = "# " + title + "\n" + name + "\n" + badge + "\n## Description\n" + description + "\n## Table of Contents\n" + toc + "\n## Installation\n" + installation + "\n## Usage\n" + usage + "\n## License\n" + licensing + "\n" + licenseDesc + "\n## Contributing\n" + contributing + "\n## Tests\n" + tests + "\n## Questions" + username + email;
     writeToFile(readMe);
 });
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(readMe) {
+    // Create "Generated" file to retain repo's README.md
     fs.writeFile("README(GENERATED).md", readMe, (err) => {
         if (err)
           console.log(err);
